cd972833c4f761b9cdbe6465398d801c21f0cd79
Cahya <cahyabintangp@gmail.com>
2024-01-22 07:09:04 +0700

    update riwayat

--- resources/views/user/dashboard/riwayat.blade.php
+++ resources/views/user/dashboard/riwayat.blade.php
@@ -7,20 +7,7 @@
 
         <div class="relative overflow-x-auto pt-2">
             <div
-                class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-gray-100 dark:bg-gray-900">
-                <button type="button"
-                    class="bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <div class="flex gap-4 py-2 px-3 rounded-md text-white">
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                            viewBox="0 0 20 16">
-                            <path
-                                d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
-                            <path
-                                d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
-                        </svg>
-                        <p class="font-semibold">Pesan</p>
-                    </div>
-                </button>
+                class="flex items-center justify-end flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-gray-100 dark:bg-gray-900">
                 <label for="table-search" class="sr-only">Search</label>
                 <div class="relative">
                     <form role="search" id="form" class="flex items-center relative">
